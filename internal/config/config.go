bb01161aa02a9295470d06e51148b668309c555b
vsimonari <vsimonari@gmail.com>
2024-10-13 20:57:09 +0700

    Added config structs for database connection

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ import (
 
 type BaseConfig struct{}
 
-func (c *BaseConfig) getBoolEnv(name string) bool {
+func getBoolEnv(name string) bool {
 	value_as_string, is_present := os.LookupEnv(name)
 
 	if !is_present {
@@ -24,7 +24,7 @@ func (c *BaseConfig) getBoolEnv(name string) bool {
 	return value
 }
 
-func (c *BaseConfig) getStrEnv(name string) string {
+func getStrEnv(name string) string {
 	value, is_present := os.LookupEnv(name)
 
 	if !is_present {
@@ -34,7 +34,7 @@ func (c *BaseConfig) getStrEnv(name string) string {
 	return value
 }
 
-func (c *BaseConfig) getIntEnv(name string) int {
+func getIntEnv(name string) int {
 	value_as_string, is_present := os.LookupEnv(name)
 
 	if !is_present {
@@ -50,57 +50,12 @@ func (c *BaseConfig) getIntEnv(name string) int {
 	return value
 }
 
-type DatabaseCredentialsConfig struct {
-	BaseConfig
-	USER     string
-	PASSWORD string
-}
-
-func (c *DatabaseCredentialsConfig) New() *DatabaseCredentialsConfig {
-	return &DatabaseCredentialsConfig{
-		USER:     c.getStrEnv("POSTGRES_USER"),
-		PASSWORD: c.getStrEnv("POSTGRES_PASSWORD"),
-	}
-}
-
-type DatabaseConnectionConfig struct {
-	BaseConfig
-	HOST     string
-	PORT     int
-	DATABASE string
-}
-
-func (c *DatabaseConnectionConfig) New() *DatabaseConnectionConfig {
-	return &DatabaseConnectionConfig{
-		HOST:     c.getStrEnv("POSTGRES_HOST"),
-		PORT:     c.getIntEnv("POSTGRES_PORT"),
-		DATABASE: c.getStrEnv("POSTGRES_DATABASE"),
-	}
-}
-
-type DatabaseDetailsConfig struct {
-	BaseConfig
-	SSLMode  bool
-	Timezone string
-}
-
-func (c *DatabaseDetailsConfig) New() *DatabaseDetailsConfig {
-	return &DatabaseDetailsConfig{
-		SSLMode:  c.getBoolEnv("POSTGRES_SSL_MODE"),
-		Timezone: c.getStrEnv("POSTGRES_TIMEZONE"),
-	}
-}
-
-type DatabaseConfig struct {
-	Credentials DatabaseCredentialsConfig
-	Connection  DatabaseConnectionConfig
-	Details     DatabaseDetailsConfig
-}
-
-func (c *DatabaseConfig) MakeDSN() {
-
-}
-
 type Config struct {
 	Database DatabaseConfig
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Database: *newDatabaseConfig(),
+	}
 }
--- internal/config/database.go
+++ internal/config/database.go
@@ -1,1 +1,67 @@
 package config
+
+import "fmt"
+
+type DatabaseCredentialsConfig struct {
+	USER     string
+	PASSWORD string
+}
+
+type DatabaseConnectionConfig struct {
+	HOST     string
+	PORT     int
+	DATABASE string
+}
+
+type DatabaseDetailsConfig struct {
+	SSLMode  string
+	Timezone string
+}
+
+func getSSLModeEnv() string {
+	map_ := map[bool]string{
+		true:  "enable",
+		false: "disable",
+	}
+
+	value := getBoolEnv("POSTGRES_SSL_MODE")
+
+	return map_[value]
+}
+
+type DatabaseConfig struct {
+	Credentials DatabaseCredentialsConfig
+	Connection  DatabaseConnectionConfig
+	Details     DatabaseDetailsConfig
+}
+
+func (c *DatabaseConfig) MakeDSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s TimeZone=%s",
+		c.Connection.HOST,
+		c.Connection.PORT,
+		c.Credentials.USER,
+		c.Credentials.PASSWORD,
+		c.Connection.DATABASE,
+		c.Details.SSLMode,
+		c.Details.Timezone,
+	)
+}
+
+func newDatabaseConfig() *DatabaseConfig {
+	return &DatabaseConfig{
+		Credentials: DatabaseCredentialsConfig{
+			USER:     getStrEnv("POSTGRES_USER"),
+			PASSWORD: getStrEnv("POSTGRES_PASSWORD"),
+		},
+		Connection: DatabaseConnectionConfig{
+			HOST:     getStrEnv("POSTGRES_HOST"),
+			PORT:     getIntEnv("POSTGRES_PORT"),
+			DATABASE: getStrEnv("POSTGRES_DATABASE"),
+		},
+		Details: DatabaseDetailsConfig{
+			SSLMode:  getSSLModeEnv(),
+			Timezone: getStrEnv("POSTGRES_TIMEZONE"),
+		},
+	}
+}
