47f2b1ecbda3a92abadc14c23473d793b643c1da
vsimonari <vsimonari@gmail.com>
2024-10-21 08:27:46 +0700

    Changed main.go file

--- cmd/poker-backend/main.go
+++ cmd/poker-backend/main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"cmd/poker-backend/internal/config"
+	"cmd/poker-backend/internal/database"
+	"cmd/poker-backend/internal/poker"
+
+	"log"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
 func main() {
+	err := godotenv.Load("./.env")
+
+	if err != nil {
+		log.Fatalf("%e", err)
+	}
+
+	cfg := config.NewConfig()
+
+	db := database.NewConnection(cfg.Database)
+	migrateDatabaseAtStartup(db)
+
+	router := poker.SetUpRouter(db)
+	router.Run()
+}
+
+func migrateDatabaseAtStartup(db *gorm.DB) {
+	db.AutoMigrate(
+		&database.User{},
+		&database.Room{},
+		&database.RoomUserRef{},
+		&database.Task{},
+		&database.RoomTasksRef{},
+		&database.Estimate{},
+		&database.TaskEstimatesRef{},
+	)
 }
