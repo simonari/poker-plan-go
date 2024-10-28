79a6538bd8c3bcbaa5321600f9834bc5fb6f4e5d
vsimonari <vsimonari@gmail.com>
2024-10-28 08:11:39 +0700

    auth config made nested for further methods implementation

--- internal/config/auth.go
+++ internal/config/auth.go
@@ -1,11 +1,19 @@
 package config
 
 type AuthConfig struct {
-	TokenLifespanHours int
+	JwtTokenConfig *JwtTokenConfig
+}
+
+type JwtTokenConfig struct {
+	LifespanHours int
+	Issuer        string
 }
 
 func newAuthConfig() *AuthConfig {
 	return &AuthConfig{
-		TokenLifespanHours: getIntEnv("TOKEN_LIFESPAN_HOURS"),
+		JwtTokenConfig: &JwtTokenConfig{
+			LifespanHours: getIntEnv("JWT_TOKEN_LIFESPAN_HOURS"),
+			Issuer:        getStrEnv("JWT_TOKEN_ISSUER"),
+		},
 	}
 }
